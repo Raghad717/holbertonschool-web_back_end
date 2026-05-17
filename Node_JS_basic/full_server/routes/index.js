4f7f30ab0f6cfc18ae7047c82dbb6c07255f4bba
Raghad717 <r4gh4dalmalki@gmail.com>
2026-05-17 11:22:18 +0300

    Refactor routes to use ES6 import/export syntax

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,11 +1,11 @@
-const AppController = require('../controllers/AppController');
-const StudentsController = require('../controllers/StudentsController');
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
 
-const express = require('express');
-const router = express.Router();
+const router = Router();
 
 router.get('/', AppController.getHomepage);
 router.get('/students', StudentsController.getAllStudents);
 router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
-module.exports = router;
+export default router;
