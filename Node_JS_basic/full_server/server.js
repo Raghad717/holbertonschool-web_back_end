815a7a6050038a823264809ec23863afe5a9982f
Raghad717 <r4gh4dalmalki@gmail.com>
2026-05-17 11:12:34 +0300

    Change import/export syntax to CommonJS

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import router from './routes';
+const express = require('express');
+const routes = require('./routes');
 
 const app = express();
+const port = 1245;
 
-app.use('/', router);
+app.use('/', routes);
 
-const PORT = 1245;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 
-app.listen(PORT);
-
-export default app;
+module.exports = app;
