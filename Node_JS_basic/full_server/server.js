de8163b311c2333179754ed92544cfb99f95fd5c
Raghad717 <r4gh4dalmalki@gmail.com>
2026-05-17 11:22:52 +0300

    Refactor server.js to use ES6 module syntax

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,13 +1,10 @@
-const express = require('express');
-const routes = require('./routes');
+import express from 'express';
+import router from './routes/index';
 
 const app = express();
-const port = 1245;
 
-app.use('/', routes);
+app.use('/', router);
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+app.listen(1245);
 
-module.exports = app;
+export default app;
